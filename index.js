9e1f99cae3a7856f7f18797d74c818c0e0fd0569
Ahmed Arafa <ahmedarafa.software.developer@gmail.com>
2026-01-28 22:26:13 +0200

    Third commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const main = require("./db/connection");
 const app = express();
 const taskRouter = require("./routes/tasks.route");
 
-
+main();
 // Serve static files from public folder
 app.use(express.static("./public"));
 app.use(express.json());
@@ -22,11 +22,6 @@ const port = process.env.PORT || 3000
 
 
 // Connect to MongoDB and start server
-main().then(() => {
-  app.listen(port, () => {
-    console.log(`server is running on port ${port}`);
-  });
-}).catch(err => {
-  console.error('Failed to connect to MongoDB:', err);
-  process.exit(1);
-});
+app.listen(port , ()=>{
+  console.log(`server is running on port ${port}`);
+})
